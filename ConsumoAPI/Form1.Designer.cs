635fb6e2e5af6b61d0bb3bb01b2db27d9e1a09ae
melo-3 <gio.kitcat@gmail.com>
2023-05-31 17:01:36 -0300

    Adicionar arquivos de projeto.

---
+++ ConsumoAPI/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ConsumoAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtSobrenome = new System.Windows.Forms.TextBox();
+            this.txtFantasia = new System.Windows.Forms.TextBox();
+            this.txtLocal = new System.Windows.Forms.TextBox();
+            this.btnAddP = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.MediumPurple;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(259, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(529, 283);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sobrenome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Fantasia";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Local";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(116, 101);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 5;
+            // 
+            // txtSobrenome
+            // 
+            this.txtSobrenome.Location = new System.Drawing.Point(116, 131);
+            this.txtSobrenome.Name = "txtSobrenome";
+            this.txtSobrenome.Size = new System.Drawing.Size(100, 20);
+            this.txtSobrenome.TabIndex = 6;
+            // 
+            // txtFantasia
+            // 
+            this.txtFantasia.Location = new System.Drawing.Point(116, 162);
+            this.txtFantasia.Name = "txtFantasia";
+            this.txtFantasia.Size = new System.Drawing.Size(100, 20);
+            this.txtFantasia.TabIndex = 7;
+            // 
+            // txtLocal
+            // 
+            this.txtLocal.Location = new System.Drawing.Point(116, 193);
+            this.txtLocal.Name = "txtLocal";
+            this.txtLocal.Size = new System.Drawing.Size(100, 20);
+            this.txtLocal.TabIndex = 8;
+            // 
+            // btnAddP
+            // 
+            this.btnAddP.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnAddP.FlatAppearance.BorderSize = 0;
+            this.btnAddP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddP.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAddP.Location = new System.Drawing.Point(65, 234);
+            this.btnAddP.Name = "btnAddP";
+            this.btnAddP.Size = new System.Drawing.Size(138, 32);
+            this.btnAddP.TabIndex = 9;
+            this.btnAddP.Text = "Adicionar personagem";
+            this.btnAddP.UseVisualStyleBackColor = false;
+            this.btnAddP.Click += new System.EventHandler(this.btnAddP_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.MediumPurple;
+            this.label5.Location = new System.Drawing.Point(83, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 28);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "API RPG";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 381);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnAddP);
+            this.Controls.Add(this.txtLocal);
+            this.Controls.Add(this.txtFantasia);
+            this.Controls.Add(this.txtSobrenome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtSobrenome;
+        private System.Windows.Forms.TextBox txtFantasia;
+        private System.Windows.Forms.TextBox txtLocal;
+        private System.Windows.Forms.Button btnAddP;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ConsumoAPI/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Data;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace ConsumoAPI
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient httpClient;
+        private DataTable dataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            httpClient= new HttpClient();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                //executa o codigo se der erro vai pro catch
+
+                string linkapi = "http://localhost:5126/api/Personagens";
+                HttpResponseMessage response = await httpClient.GetAsync(linkapi);
+
+                if(response.IsSuccessStatusCode)
+                {
+                    string conteudo = await response.Content.ReadAsStringAsync();
+                    var lista = JsonConvert.DeserializeObject<List<Personagem>>(conteudo);
+                    CriarEstrutura(lista);
+                }
+                else
+                {
+                    MessageBox.Show($"Erro ao consumir API: {response.StatusCode}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro: {ex.Message}");
+                //executa outro codigo 
+
+            }
+        }
+
+        private void CriarEstrutura(List<Personagem> listaObj)
+        {
+            //dataGridView1
+            dataTable= new DataTable();
+            dataTable.Columns.Add("Nome", typeof(string));
+            dataTable.Columns.Add("Sobrenome",typeof(string));
+            dataTable.Columns.Add("Fantasia",typeof(string));
+            dataTable.Columns.Add("Local",typeof(string));
+
+            foreach (var item in listaObj)
+            {
+                dataTable.Rows.Add(item.Nome, item.Sobrenome, item.Fantasia, item.Local);
+            }
+            dataGridView1.DataSource = dataTable;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        public class Personagem
+        {
+            public int Id { get; set;}
+            public string Nome { get; set;} 
+            public string Sobrenome { get; set;}
+            public string Fantasia { get; set;}
+            public string Local { get; set;}
+        }
+
+        private async void btnAddP_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string sobrenome = txtSobrenome.Text;
+            string fantasia = txtFantasia.Text;
+            string local = txtLocal.Text;
+
+            string urlApi = "http://localhost:5126/api/Personagens";
+
+            var parametros = new Dictionary<string, string>
+            {
+                {"Id","0" }, //problema pq id é int -> solução: lista
+                { "Nome", nome },
+                { "Sobrenome", sobrenome },
+                { "Fantasia", fantasia },
+                { "Local", local }
+            };
+
+            var content = new FormUrlEncodedContent(parametros);
+            var response = await httpClient.PostAsync(urlApi, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                txtNome.Text = "";
+                txtSobrenome.Text = "";
+                txtFantasia.Text = "";
+                txtLocal.Text = "";
+
+            }
+        }
+    }
+}
